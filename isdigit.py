2cb76bec46eeffe33db3c4ec2ff7fdd064e56c62
mondmann <mondmann@users.noreply.github.com>
2018-12-10 10:35:35 +0100

    Beispiele für PP

---
+++ isdigit.py
@@ -0,0 +1,7 @@
+# Prüfen auf Zahleneingabe
+
+user_input = input("Eingabe: ")
+if user_input.isdigit():
+    print("Eingabe ist eine ganze Zahl")
+else:
+    print("Eingabe ist keine ganze Zahl")
---
+++ randint.py
@@ -0,0 +1,6 @@
+# Ermitteln von zufälligen ganzen Zahlen
+
+import random # am Anfang der Datei
+
+random_number = random.randint(10,20) # Zufallszahl von 10 bis 20 (einschließlich)
+print(random_number)
