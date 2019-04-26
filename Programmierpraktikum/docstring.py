bde9f1d35ee57db8502687d3d48e1d1089ecb820
hananja <e.loos@schule.bremen.de>
2019-04-26 09:39:08 +0200

    Docstrings

---
+++ Programmierpraktikum/docstring.py
@@ -0,0 +1,11 @@
+# Demo für Docstrings
+
+
+def f(a: int, b: int) -> int:
+    """ This function demonstrates docstrings.
+
+    :param a: first parameter to add
+    :param b: second parameter to add
+    :return: sum of parameter a and b
+    """
+    return a + b
