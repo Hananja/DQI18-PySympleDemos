5e016873caa74b3c5f49dd58daa7cdc0688191c4
Elmar Loos <e.loos@schule.bremen.de>
2019-10-01 10:40:07 +0200

    itertools Beispiel

---
+++ Programmierpraktikum/iter_chain.py
@@ -0,0 +1,18 @@
+# test with itertools.chain(...)
+
+import itertools
+
+filenames = ["test%02d.txt" % (i + 1) for i in range(3)]
+for filename in filenames:
+    with open(filename, "w") as testfile:
+        testfile.writelines(["test %02d\n" % (i + 1) for i in range(3)])
+
+testfiles = [open(filename) for filename in filenames]
+
+# Hintereinanderhaengen mit itertools
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+result = itertools.chain(testfiles[0], testfiles[1], testfiles[2])
+
+for line in result:
+    print(line)
+map(lambda f: f.close(), testfiles)
