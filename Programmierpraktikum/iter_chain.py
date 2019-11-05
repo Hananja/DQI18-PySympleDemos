e7b5f70c977124a64c8f2a144657e77a0500bfdc
hananja <e.loos@schule.bremen.de>
2019-11-05 08:50:05 +0100

    Mehr Kommentare, Code cleanup

--- Programmierpraktikum/iter_chain.py
+++ Programmierpraktikum/iter_chain.py
@@ -1,19 +1,20 @@
 # Demo von itertools.chain(...)
 import contextlib
 import itertools
-
-# Testdateien anlegen: 3 Dateien mit jeweils drei Zeilen Text
 import sys
 
 filenames = ["test%02d.txt" % (i + 1) for i in range(3)]
+
+# Testdateien anlegen: 3 Dateien mit jeweils drei Zeilen Text
 try:
     for filename in filenames:
         with open(filename, "w") as testfile:
             testfile.writelines(["test %02d\n" % (i + 1) for i in range(3)])
-except OSError as e:
+except OSError as e: # Fehlerbehandlung
     print("%s: %s" %(e.strerror, e.filename), file=sys.stderr)
     exit(1)
 
+# Dateien Auslesen (Hauptteil)
 try:
     # Dateien zum Lesen oeffnen
     with contextlib.ExitStack() as exit_stack:
@@ -27,6 +28,6 @@ try:
         # iterieren ueber das Ergebnis
         for line in result:
             print(line, end="") # Newlines sind schon vorhanden
-except OSError as e:
+except OSError as e: # Fehlerbehandlung
     print("%s: %s" %(e.strerror, e.filename), file=sys.stderr)
     exit(2)
