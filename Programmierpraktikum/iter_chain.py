8d4c3eacf290995fa779be5d50b4e5823885f3c5
hananja <e.loos@schule.bremen.de>
2019-11-05 08:42:49 +0100

    Fehlerbehandlung und with statement mit Listen

--- Programmierpraktikum/iter_chain.py
+++ Programmierpraktikum/iter_chain.py
@@ -1,23 +1,32 @@
 # Demo von itertools.chain(...)
-
+import contextlib
 import itertools
 
 # Testdateien anlegen: 3 Dateien mit jeweils drei Zeilen Text
+import sys
+
 filenames = ["test%02d.txt" % (i + 1) for i in range(3)]
-for filename in filenames:
-    with open(filename, "w") as testfile:
-        testfile.writelines(["test %02d\n" % (i + 1) for i in range(3)])
+try:
+    for filename in filenames:
+        with open(filename, "w") as testfile:
+            testfile.writelines(["test %02d\n" % (i + 1) for i in range(3)])
+except OSError as e:
+    print("%s: %s" %(e.strerror, e.filename), file=sys.stderr)
+    exit(1)
 
-# Dateien zum Lesen oeffnen
-testfiles = [open(filename) for filename in filenames]
+try:
+    # Dateien zum Lesen oeffnen
+    with contextlib.ExitStack() as exit_stack:
+        testfiles = [exit_stack.enter_context(open(filename)) for filename in filenames]
 
-# Hintereinanderhaengen mit itertools
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-result = itertools.chain(testfiles[0], testfiles[1], testfiles[2])
+        # Hintereinanderhaengen mit itertools
+        # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        # result = itertools.chain(testfiles[0], testfiles[1], testfiles[2])
+        result = itertools.chain(*testfiles)
 
-# iterieren ueber das Ergebnis
-for line in result:
-    print(line, end="") # Newlines sind schon vorhanden
-
-# Dateien alle wieder schliessen
-map(lambda f: f.close(), testfiles)
+        # iterieren ueber das Ergebnis
+        for line in result:
+            print(line, end="") # Newlines sind schon vorhanden
+except OSError as e:
+    print("%s: %s" %(e.strerror, e.filename), file=sys.stderr)
+    exit(2)
